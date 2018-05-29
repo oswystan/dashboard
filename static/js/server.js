8cbeecd7c6e9b4a64b8ac15607b3b285a981ec68
oswystan <oswystan@126.com>
2018-05-29 12:18:33 +0800

    feature: add stream detail diag

--- static/js/server.js
+++ static/js/server.js
@@ -2,6 +2,28 @@
     let jq_main_server = $("#main_servers");
     let jq_client = $(".server-stat a");
     let jq_main_client = $("#main_clients");
+    let jq_stream = $("#main_clients a");
+    let jq_diag_mask = $("#diag_mask");
+    let jq_diag = $("#diag_mask>*");
+
+    function show_diag(isShow) {
+        if (isShow) {
+            jq_diag_mask.css("display", "grid");
+        } else {
+            jq_diag_mask.hide();
+        }
+    }
+
+    jq_stream.unbind('click').click(()=>{
+        show_diag(true);
+    });
+    jq_diag_mask.unbind('click').click(()=>{
+        show_diag(false);
+    });
+    jq_diag.unbind('click').click(()=>{
+        return false;
+    });
+
     jq_client.unbind("click").click(function(){
         console.log("show client detail");
         jq_main_server.hide();
